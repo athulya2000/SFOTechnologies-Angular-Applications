548a31958839176a5583fe800b0e55aee6ea2c09
athulya2000 <athulyav00@gmail.com>
2022-12-04 16:51:58 +0530

    navbar3 completed

--- src/app/employee-login/employee-login.component.ts
+++ src/app/employee-login/employee-login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-employee-login',
@@ -10,9 +12,16 @@ export class EmployeeLoginComponent {
   employeeid=""
   password=""
 
+  constructor(private route:Router){}
+
   readValues=()=>
   {
     let login:any={"employeeid":this.employeeid,"password":this.password}
     console.log(login)
+    if (this.employeeid=="1122" && this.password=="12345") {
+      this.route.navigate(['/addcourse'])
+    } else {
+      alert("invalid credentials")
+    }
   }
 }
---
+++ src/app/nav-bar3/nav-bar3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar3',
+  templateUrl: './nav-bar3.component.html',
+  styleUrls: ['./nav-bar3.component.css']
+})
+export class NavBar3Component {
+
+}
